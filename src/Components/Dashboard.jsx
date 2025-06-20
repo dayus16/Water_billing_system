9e02a90aab27515c587dbc31f1f51ae4c4359bda
DAYO ODOJE <dayodoje@gmail.com>
2025-06-20 10:42:06 -0400

    call state, lga and town endpoints and created taxpayers

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -6,11 +6,11 @@ const Dashboard = () => {
   return (
     <div className="grow p-8">
       <h2 className="text-2xl mb-4">Dashboard</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mb-6">
         <Card icon={<FaUser />} title="Taxpayer" value="10" />
         <Card icon={<FaBuilding  />} title="Bills" value="3" />
         <Card icon={<FaBox />} title="Building" value="4" />
-        <Card icon={<FaCog />} title="Settings" value="11" />
+        {/* <Card icon={<FaCog />} title="Settings" value="11" /> */}
       </div>
     </div>
   );
--- src/Components/Taxpayer.jsx
+++ src/Components/Taxpayer.jsx
@@ -1,10 +1,108 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import { IoMdAdd } from "react-icons/io";
+import { toast } from "react-toastify";
 
 const Taxpayer = () => {
   const [showModal, setShowModal] = useState(false);
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [street, setStreet] = useState("");
+  const [address, setAddress] = useState("");
+  const [state, setState] = useState("");
+  const [lga, setLga] = useState("");
+  const [town, setTown] = useState("");
+  const [employmentStatus, setEmploymentStatus] = useState("");
+  const [stateList, setStateList] = useState([]);
+  const [lgaList, setLgaList] = useState([]);
+  const [townList, setTownList] = useState([]);
+  const [result, setResult] = useState("")
+
+  const handleSaveBtn = async (e) => {
+    e.preventDefault();
+    setResult('Loading....')
+    try {
+      const res = await fetch(
+        `https://water-billing-72y7.onrender.com/api/v1/taxpayers`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            fullname,
+            email,
+            phone_number: phoneNumber,
+            street,
+            address,
+            state_id: state,
+            lga_id: lga,
+            town_id: town,
+            employment_status: employmentStatus,
+          }),
+        }
+      );
+      const data = await res.json();
+      console.log(data);
+      if (res.ok) {
+        toast.success("taxpayer created successfully");
+        setResult("")
+        console.log("taxpayer created successfully");
+      } else {
+        toast.error(data.message);
+        console.log(data.message);
+        setResult("Save")
+      }
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  };
+
+  const handleState = async () => {
+    try {
+      const res = await fetch(
+        "https://water-billing-72y7.onrender.com/api/v1/states"
+      );
+      const data = await res.json();
+      console.log(data.states);
+      setStateList(data.states);
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  };
+
+  useEffect(() => {
+    handleState();
+  }, []);
+
+  const handleLga = async (stateId) => {
+    try {
+      const res = await fetch(
+        `https://water-billing-72y7.onrender.com/api/v1/lgas/lga/${stateId}`
+      );
+
+      const data = await res.json();
+      console.log("LGAs fetched:", data.lgas);
+      setLgaList(data.lgas);
+    } catch (err) {
+      console.error("Failed to fetch LGAs:", err);
+    }
+  };
+
+  const handleTown = async (lgaId) => {
+    try {
+      const res = await fetch(
+        `https://water-billing-72y7.onrender.com/api/v1/towns/${lgaId}`
+      );
+      const data = await res.json();
+      console.log("Towns fetched:", data.townDetails);
+      setTownList(data.townDetails);
+    } catch (err) {
+      console.error("Failed to fetch towns:", err);
+    }
+  };
 
   return (
     <div className="flex">
@@ -43,7 +141,7 @@ const Taxpayer = () => {
                   </button>
                 </div>
 
-                <form className="space-y-4">
+                <form className="space-y-4" onSubmit={handleSaveBtn}>
                   <div className="text-gray-700">
                     <label
                       htmlFor="text"
@@ -56,6 +154,8 @@ const Taxpayer = () => {
                       id="email"
                       className="w-full py-1 px-3 border outline-blue-200 rounded"
                       placeholder="Full Name"
+                      value={fullname}
+                      onChange={(e) => setFullname(e.target.value)}
                       required
                     />
                   </div>
@@ -72,6 +172,8 @@ const Taxpayer = () => {
                         id="email"
                         className="w-full py-1 px-3 border outline-blue-200 rounded"
                         placeholder="name@example.com"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         required
                       />
                     </div>
@@ -83,10 +185,12 @@ const Taxpayer = () => {
                         Phone Number
                       </label>
                       <input
-                        type="number"
+                        type="text"
                         id="phoneNumber"
                         className="w-full py-1 px-3  outline-blue-200 border rounded"
                         placeholder="Phone Number"
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
                         required
                       />
                     </div>
@@ -103,6 +207,8 @@ const Taxpayer = () => {
                       id="street"
                       className="w-full py-1 px-3  outline-blue-200 border rounded"
                       placeholder="street"
+                      value={street}
+                      onChange={(e) => setStreet(e.target.value)}
                       required
                     />
                   </div>
@@ -118,22 +224,63 @@ const Taxpayer = () => {
                       id="Address"
                       className="w-full py-1 px-3  outline-blue-200 border rounded"
                       placeholder="Address"
+                      value={address}
+                      onChange={(e) => setAddress(e.target.value)}
                       required
                     />
                   </div>
                   <div className="flex gap-2">
-                    <select className="flex-1 py-1 px-3 border  outline-blue-200 rounded text-gray-700">
+                    <select
+                      className="flex-1 py-1 px-3 border  outline-blue-200 rounded text-gray-700"
+                      value={state}
+                      onChange={(e) => {
+                        const selectedState = e.target.value;
+                        setState(selectedState);
+                        handleLga(selectedState);
+                      }}
+                    >
                       <option value="">-- Select a state --</option>
+                      {stateList.map((item) => (
+                        <option key={item.id} value={item.id}>
+                          {item.state}
+                        </option>
+                      ))}
                     </select>
-                    <select className="flex-1 py-1 px-3 border  outline-blue-200 rounded text-gray-700">
+                    <select
+                      className="flex-1 py-1 px-3 border  outline-blue-200 rounded text-gray-700"
+                      value={lga}
+                      onChange={(e) => {
+                        const selectedLgaId = e.target.value;
+                        setLga(selectedLgaId);
+                        handleTown(selectedLgaId);
+                      }}
+                    >
                       <option value="">-- Select an LGA --</option>
+                      {lgaList.map((lgaItem) => (
+                        <option key={lgaItem.id} value={lgaItem.id}>
+                          {lgaItem.lga}
+                        </option>
+                      ))}
                     </select>
                   </div>
                   <div className="flex gap-2">
-                    <select className="w-full py-1 px-3 border border-gray-300  outline-blue-200 rounded text-gray-700">
+                    <select
+                      className="w-full py-1 px-3 border border-gray-300  outline-blue-200 rounded text-gray-700"
+                      value={town}
+                      onChange={(e) => setTown(e.target.value)}
+                    >
                       <option value="">-- Select a town --</option>
+                      {townList.map((townItem) => (
+                        <option key={townItem.id} value={townItem.id}>
+                          {townItem.town}
+                        </option>
+                      ))}
                     </select>
-                    <select className="w-full py-1 px-3 border border-gray-300  outline-blue-200 rounded text-gray-700">
+                    <select
+                      className="w-full py-1 px-3 border border-gray-300  outline-blue-200 rounded text-gray-700"
+                      value={employmentStatus}
+                      onChange={(e) => setEmploymentStatus(e.target.value)}
+                    >
                       <option value="">-- Employment status --</option>
                       <option value="employed">Employed</option>
                       <option value="unemployed">Unemployed</option>
@@ -145,7 +292,7 @@ const Taxpayer = () => {
                     type="submit"
                     className="w-full bg-blue-700 text-white py-2 font-semibold cursor-pointer rounded hover:bg-blue-800"
                   >
-                    Save
+                    {result ? result : 'Save'}
                   </button>
                 </form>
               </div>
@@ -154,31 +301,44 @@ const Taxpayer = () => {
         </div>
 
         {/* --------------Table--------------------------- */}
-       <div className="p-7 overflow-x-auto">
-  <table className="min-w-[800px] w-full text-center text-sm font-light text-surface dark:text-white">
-    <thead className="border-b border-neutral-200 bg-neutral-50 font-medium dark:border-white/10 dark:text-neutral-800">
-      <tr>
-        <th scope="col" className="px-6 py-4">#</th>
-        <th scope="col" className="px-6 py-4">Fullname</th>
-        <th scope="col" className="px-6 py-4">Phone No.</th>
-        <th scope="col" className="px-6 py-4">Email</th>
-        <th scope="col" className="px-6 py-4">State</th>
-        <th scope="col" className="px-6 py-4">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr className="border-b border-neutral-200 dark:border-white/10">
-        <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
-        <td className="whitespace-nowrap px-6 py-4">Mark</td>
-        <td className="whitespace-nowrap px-6 py-4">08012345678</td>
-        <td className="whitespace-nowrap px-6 py-4">mark@example.com</td>
-        <td className="whitespace-nowrap px-6 py-4">Lagos</td>
-        <td className="whitespace-nowrap px-6 py-4">Edit | Delete</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-
+        <div className="p-7 overflow-x-auto">
+          <table className="min-w-[800px] w-full text-center text-sm font-light text-surface dark:text-white">
+            <thead className="border-b border-neutral-200 bg-neutral-50 font-medium dark:border-white/10 dark:text-neutral-800">
+              <tr>
+                <th scope="col" className="px-6 py-4">
+                  #
+                </th>
+                <th scope="col" className="px-6 py-4">
+                  Fullname
+                </th>
+                <th scope="col" className="px-6 py-4">
+                  Phone No.
+                </th>
+                <th scope="col" className="px-6 py-4">
+                  Email
+                </th>
+                <th scope="col" className="px-6 py-4">
+                  State
+                </th>
+                <th scope="col" className="px-6 py-4">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="border-b border-neutral-200 dark:border-white/10">
+                <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
+                <td className="whitespace-nowrap px-6 py-4">Mark</td>
+                <td className="whitespace-nowrap px-6 py-4">08012345678</td>
+                <td className="whitespace-nowrap px-6 py-4">
+                  mark@example.com
+                </td>
+                <td className="whitespace-nowrap px-6 py-4">Lagos</td>
+                <td className="whitespace-nowrap px-6 py-4">Edit | Delete</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
